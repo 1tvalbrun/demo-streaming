4e6ec3fbcdcf1e6cf3afd712ee2fbd3882a3b890
Thierry Valbrun <1tvalbrun@gmail.com>
2021-02-23 12:25:48 -0500

    App setup

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+const App = () => {
+  return (
+    <React.Fragment>
+      <h1>This is just the beginning</h1>
+    </React.Fragment>
+  );
+};
+
+export default App;
